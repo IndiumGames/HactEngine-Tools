47cc1449b78b88da53a04abeed683896c0008758
Sami Kankaristo <sami@indiumgames.fi>
2014-02-12 12:42:52 +0200

    I wrote a small Bash script, which runs SWIG into a temporary file, makes a diff of the 2 files and replaces the file only if necessary.
    Former-commit-id: 11805197b01a5a0bb7c7ea31ed6894e39aa91303 [formerly 7d7432632891427773b5200c51dfaae06ab626e8]
    Former-commit-id: 4f642d7644eff341a8f33ba3d570e436941a0dfb

---
+++ swig/swig_diff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -n "$1" ]]; then
+    cd $1
+fi
+
+swig -c++ -lua -o swig_wrap.temp swig.i
+
+if ! diff -q swig_wrap.temp swig_wrap.cxx > /dev/null ; then
+    rm swig_wrap.cxx
+    mv swig_wrap.temp swig_wrap.cxx
+else
+    rm swig_wrap.temp
+fi
