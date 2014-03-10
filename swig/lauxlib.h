56f9073732b7f9be5212c636e7d8c0c96c5970bc
Sami Kankaristo <sami@indiumgames.fi>
2014-03-10 21:38:44 +0200

    A bit of progress on the Android build, but it's not working quite yet...
    Former-commit-id: c1c61f3daa896e90171821ff867232f4abafd394 [formerly 60004d9063978ee3d5b4445e7f692e9d9089bf3b]
    Former-commit-id: 207a738504dbd44506c0d527412634f44a471f8f

--- swig/lauxlib.h
+++ swig/lauxlib.h
@@ -1,9 +1,9 @@
-#if defined HACT_ANDROID
-    #include "lib/android/lua/include/lauxlib.h"
-#elif defined HACT_UNIX
-    //#include <lua5.2/lauxlib.h>
+//#if defined HACT_ANDROID
+//    #include "lib/android/lua/include/lauxlib.h"
+//#elif defined HACT_UNIX
+//    //#include <lua5.2/lauxlib.h>
     #include <lauxlib.h>
-#elif defined HACT_WINDOWS
-    #include <lauxlib.h>
-#endif
+//#elif defined HACT_WINDOWS
+//    #include <lauxlib.h>
+//#endif
 
--- swig/lua.h
+++ swig/lua.h
@@ -1,9 +1,9 @@
-#if defined HACT_ANDROID
-    #include "lib/android/lua/include/lua.h"
-#elif defined HACT_UNIX
-    //#include <lua5.2/lua.h>
+//#if defined HACT_ANDROID
+//    #include "lib/android/lua/include/lua.h"
+//#elif defined HACT_UNIX
+//    //#include <lua5.2/lua.h>
     #include <lua.h>
-#elif defined HACT_WINDOWS
-    #include <lua.h>
-#endif
+//#elif defined HACT_WINDOWS
+//    #include <lua.h>
+//#endif
 
