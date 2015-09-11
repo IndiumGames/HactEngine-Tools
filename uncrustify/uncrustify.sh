7105e741cdd48e863dc710b90e5d72191826a41e
Sami Kankaristo <sami@indiumgames.fi>
2015-09-11 14:44:55 +0300

    Fix Uncrustify script

--- uncrustify/uncrustify.sh
+++ uncrustify/uncrustify.sh
@@ -3,9 +3,12 @@
 # Directory containing this script
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Run Uncrustify
-uncrustify "$@" > /tmp/uncrustify_output
-
-# Re-indent empty lines
-lua $DIR/reindent_file.lua /tmp/uncrustify_output
-
+for file in $DIR/../../src/*; do
+    # Run Uncrustify
+    uncrustify --no-backup -c "$DIR/uncrustify.cfg" "$file"
+    
+    # Re-indent empty lines
+    lua "$DIR/reindent_file.lua" "$file" > "$file.new"
+    
+    mv "$file.new" "$file"
+done
