cc48068998d9955f8cebf4c15703d945a28ebc81
Sami Kankaristo <sami@indiumgames.fi>
2015-09-11 14:28:18 +0300

    m

--- uncrustify/uncrustify.sh
+++ uncrustify/uncrustify.sh
@@ -4,7 +4,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Run Uncrustify
-Uncrustify "$@" > /tmp/uncrustify_output
+uncrustify "$@" > /tmp/uncrustify_output
 
 # Re-indent empty lines
 lua $DIR/reindent_file.lua /tmp/uncrustify_output
