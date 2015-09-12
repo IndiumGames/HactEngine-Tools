f5e5f7cdba3795b6374c5553f4a41ba8a8dcac8a
Sami Kankaristo <sami@indiumgames.fi>
2015-09-12 18:07:54 +0300

    Fix indentation on lines after throw statements

--- uncrustify/uncrustify_fixes.lua
+++ uncrustify/uncrustify_fixes.lua
@@ -45,6 +45,27 @@ for i = 2, #lines do
     -- Uncrustify can't understand a dereference in a ranged for loop
     -- TODO: Create issue on GitHub
     lines[i] = lines[i]:gsub(": %* ", ": *")
+    
+    -- Fix indentation on lines after a throw statement
+    -- https://github.com/bengardner/uncrustify/issues/362
+    if lines[i]:find("^%s*throw") then
+        -- Get indent on the line with the throw statement
+        local throwIndent = lines[i]:match("^%s*")
+        
+        -- Loop over lines after the throw statement
+        for j = i + 1, #lines do
+            -- Get indent on current line
+            local indent = lines[j]:match("^%s*")
+            
+            -- Check that the indent is at least 6 characters longer
+            if #throwIndent <= #indent - 6 then
+                lines[j] = indent:sub(1, -(6 + 1)) .. lines[j]:match("^%s*(.*)")
+            else
+                -- Found line with smaller indent, break out of loop
+                break
+            end
+        end
+    end
 end
 
 -- Output the result
